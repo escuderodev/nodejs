9a7eb9e9f23ac1d4bebb431d1b1976e3306d0c7f
escuderodev <escuderodev@outlook.com>
2024-10-22 20:52:56 -0300

    feat: styleSheet implemented

--- react_native/my-app/App.tsx
+++ react_native/my-app/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Palmeiras não tem mundial!</Text>
+      <Text style={styles.text}>Porcos</Text>
     </View>
   );
 }
@@ -17,4 +16,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red',
+    fontSize: 22
+  }
 });
--- react_native/my-app/anotacoes.md
+++ react_native/my-app/anotacoes.md
@@ -2,4 +2,7 @@
 
 - criar app: npx create-expo-app my-app -t
 - escolher template: blank (Typescript)
+- o arquivo App.tsx é o arquivo main (entrada) do nosso projeto
+- o arquivo app.json é o arquivo de configuração do nosso projeto
+- criar pasta src
 - 
